579233911cbe2060b0d0244c79aa3b350cb3f38f
Ibai Ruiz <ibai.ruiz@liferay.com>
2019-03-14 13:14:07 +0100

    Change AZ

--- provision-nfs-filestore.sh
+++ provision-nfs-filestore.sh
@@ -3,7 +3,7 @@
 
 FS="nfs-filestore"
 PROJECT=$(gcloud config get-value project -q)
-ZONE="eu-west1-b"
+ZONE="europe-west1-b"
 gcloud beta filestore instances create ${FS} --project=${PROJECT} --location=${ZONE} --tier=STANDARD --file-share=name="volumes",capacity=1TB --network=name="default",reserved-ip-range="10.0.0.0/29"
 #FSADDR=$(gcloud beta filestore instances describe ${FS} --project=${PROJECT} --location=${ZONE} --format="value(networks.ipAddresses[0])")
 #helm install stable/nfs-client-provisioner --name nfs-cp --set nfs.server=${FSADDR} --set nfs.path=/volumes
